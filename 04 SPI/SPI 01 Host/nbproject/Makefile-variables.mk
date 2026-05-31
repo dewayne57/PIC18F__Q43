f7c1ed097a9c3296e2c9b393f1f3a064918cd9eb
Dewayne Hafenstein <dewayne@hafenstein.net>
2026-05-31 12:30:01 -0500

    spi

---
+++ 04 SPI/SPI 01 Host/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SPI_01_Host.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SPI_01_Host.production.hex
