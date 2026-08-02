c21fb6b277178f2163bb7eccab50e3865dd04a21
Dewayne Hafenstein <dewayne@hafenstein.net>
2026-08-02 15:04:34 -0500

    MPLAB X Project

---
+++ Examples/Keyboard Interface/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Keyboard_Interface.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Keyboard_Interface.production.hex
